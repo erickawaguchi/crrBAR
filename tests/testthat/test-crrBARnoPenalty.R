cbe65f5b680c7071eed4af1ad171a3eb9e108e67
Eric Kawaguchi <erickawaguchi@ucla.edu>
2018-11-28 09:02:48 -0800

    added small sample test

--- tests/testthat/test-crrBARnoPenalty.R
+++ tests/testthat/test-crrBARnoPenalty.R
@@ -1,7 +1,19 @@
 library("testthat")
 library("cmprsk")
 
-test_that("crrBAR with no penalties gives same results as crr", {
+test_that("crrBAR with no penalties gives same results as crr (small sample size)", {
+  set.seed(10)
+  ftime   <- rexp(50)
+  fstatus <- sample(0:2, 50, replace = TRUE)
+  cov     <- matrix(runif(250), nrow = 50)
+  dimnames(cov)[[2]] <- c('x1', 'x2', 'x3', 'x4', 'x5')
+
+  fit.crr    <- crr(ftime, fstatus, cov)
+  fit.crrBAR <- crrBAR(ftime, fstatus, cov, lambda = 0, xi = 0)
+  expect_equal(as.vector(fit.crr$coef), as.vector(fit.crrBAR$coef), tolerance = 1E-6)
+})
+
+test_that("crrBAR with no penalties gives same results as crr (larger sample size)", {
   set.seed(10)
   ftime   <- rexp(200)
   fstatus <- sample(0:2, 200, replace = TRUE)
@@ -10,6 +22,6 @@ test_that("crrBAR with no penalties gives same results as crr", {
 
   fit.crr    <- crr(ftime, fstatus, cov)
   fit.crrBAR <- crrBAR(ftime, fstatus, cov, lambda = 0, xi = 0)
-  expect_equal(fit.crr$coef, as.vector(fit.crrBAR$coef))
+  expect_equal(as.vector(fit.crr$coef), as.vector(fit.crrBAR$coef), tolerance = 1E-6)
 })
 
